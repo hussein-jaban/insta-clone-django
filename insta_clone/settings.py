ab4c95f179b7c7f4c7681ff68bd56523c81b50b5
Hussein Omar <husseinomar6190@gmail.com>
2022-06-04 15:34:14 +0300

    adding postgress database and installing psycopg2-2.9.3

--- insta_clone/settings.py
+++ insta_clone/settings.py
@@ -80,11 +80,23 @@ WSGI_APPLICATION = 'insta_clone.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+# Local
+DATABASES={
+   'default':{
+      'ENGINE':'django.db.backends.postgresql_psycopg2',
+      'NAME':'insta_clone',
+      'USER':'postgres',
+      'PASSWORD':'mwas6190',
+      'HOST':'localhost',
+      'PORT':'5432',
+   }
 }
 
 
