cc540045f32acb0274c4a18afd21cb5d25e87beb
Hussein Omar <husseinomar6190@gmail.com>
2022-06-04 15:29:36 +0300

    configering the templates with base app

--- base/views.py
+++ base/views.py
@@ -1,3 +1,8 @@
+import imp
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def home(request):
+   return render(request, 'home.html')
--- insta_clone/settings.py
+++ insta_clone/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'base.apps.BaseConfig',
+    'cloudinary'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +59,9 @@ ROOT_URLCONF = 'insta_clone.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates'
+            ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,6 +124,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+cloudinary.config( 
+  cloud_name = "husseincloud", 
+  api_key = "587435562281821", 
+  api_secret = "rtGxXIW3b97zR-u2GsWELp0YjVk" 
+)
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
