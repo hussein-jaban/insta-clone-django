8e8291e4ef75b9f1e95dd3162a17f881a29f6209
Hussein Omar <husseinomar6190@gmail.com>
2022-06-04 15:50:24 +0300

    creating a custom user model and migrated it to the db

--- base/models.py
+++ base/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+class User(AbstractUser):
+   name = models.CharField(max_length=200, null=True)
+   email = models.EmailField(unique=True)
+   bio = models.TextField(null=True)
+   image = CloudinaryField('image', null=True, blank=True, default='image/upload/v1654285390/hz7a08c74kynhnx24lwd.png')
+   USERNAME_FIELD = 'email'
+   REQUIRED_FIELDS = ['username']
