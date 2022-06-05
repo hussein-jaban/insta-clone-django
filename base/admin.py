3e229ebfefa0d739dbf90b6b0ac7d72f99ce7c1d
Hussein Omar <husseinomar6190@gmail.com>
2022-06-05 21:21:08 +0300

    creating a follow count model to keep records of followers activities

--- base/admin.py
+++ base/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import User, Post, Comment, AllLikes
+from .models import User, Post, Comment, AllLikes, FollowersCount
 # Register your models here.
 
 admin.site.register(User)
 admin.site.register(Post)
 admin.site.register(Comment)
 admin.site.register(AllLikes)
+admin.site.register(FollowersCount)
--- base/models.py
+++ base/models.py
@@ -49,6 +49,14 @@ class AllLikes(models.Model):
    
    def __str__(self):
          return self.username
+      
+      
+class FollowersCount(models.Model):
+   follower = models.CharField(max_length=200)
+   user = models.CharField(max_length=200)
+   
+   def __str__(self):
+         return self.user
    
    
    
